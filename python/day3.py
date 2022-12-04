b70c54e864f611a1bcad3815a2af9faf9aa013ac
Akash K <akashkurdekar@gmail.com>
2022-12-04 20:24:38 +0000

    part1: not working; why?

--- python/day3.py
+++ python/day3.py
@@ -3,20 +3,31 @@ def input_lines(filename):
         lines = [l.strip() for l in fp.readlines()]
     return lines
 
-def priority(dupe):
-    return 
-    
-if __name__ == "__main__":
-    lines, total = input_lines('day3_input.txt'), 0
+def priority(item):
+    return ord(item) - (96 if 'a' <= item <= 'z' else 38)
+
+def part1(lines):
+    total = 0
     for l in lines:
         half = len(l)//2
         compartment1, compartment2 = set(l[:half]), set(l[half:])
         dupe = list(compartment1.intersection(compartment2))[0]
-        offset = 96 if 'a' <= dupe <= 'z' else 38
-        total += ord(dupe) - offset
+        total += priority(dupe)
+    return total
+    
+def part2(lines):
+    total, i = 0, 0 
+    while i < len(lines):
+        common = set(lines[i]).intersection(set(lines[i+1])).intersection(set(lines[i+2]))
+        total += priority(list(common)[0])
+        i+=3
+    return total
 
-    print(total)
+if __name__ == "__main__":
+    lines = input_lines('day3_input.txt')
+    print(part1(lines))
 
+    print(part2(lines))
 
     
     
