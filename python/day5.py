87a591a78884892b1e04b4d42df0886f1060bae2
Akash K <akashkurdekar@gmail.com>
2022-12-06 08:39:08 +0000

    day5 complete

--- python/day5.py
+++ python/day5.py
@@ -10,18 +10,24 @@ with open("day5_input.txt") as f:
         'LGZDWRFQ',
         'JBWVP'
     ]]
-    i = -1
-    count=0
+    i, count = -1, 0
     for line in f:
         i += 1
         if i < 10:
             continue
         split = line.split(' ')
         count, src, dst = int(split[1]), int(split[3]), int(split[5])
-        for j in range(count):
-            num = stacks[src-1].pop()    
-            stacks[dst-1].append(num)
+        
+        ## part 1
+        # for j in range(count):
+        #     num = stacks[src-1].pop()
+        #     stacks[dst-1].append(num)
 
+        ## part 2
+        move_start_idx = len(stacks[src-1])-count
+        stacks[dst-1].extend(stacks[src-1][move_start_idx:])
+        stacks[src-1] = stacks[src-1][:move_start_idx]
+        
     str = ''
     for i in range(len(stacks)):
         str += stacks[i][-1] if len(stacks[i]) > 0 else ''
